80e5b7915f37aaa6536fd8756a1dcc4330ad31f4
Dawinartor <david.melamed@web.de>
2019-05-02 12:54:06 +0200

    Soweit alles umgeschrieben

---
+++ Game/game.js
@@ -0,0 +1,60 @@
+var game;
+var gameOptions = {
+
+}
+
+window.onload = function() {
+
+    var gameConfig = {
+        width: 900,
+        height: 900,
+        backgroundColor: 0xffffff,
+        scene: [ bootGame, playGame ]
+    }
+
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+    resizeGame();
+    window.addEventListener("resize", resizeGame);
+
+}
+
+class bootGame extends Phaser.scene {
+    constructor () {
+        super("BootGame");
+    }
+
+    preload() {
+        // Die Skyline -> Hintergrund der geladen wird.
+        this.load.image("Skyline_background.jpg", "./Assets/Sprites/Skyline_background.jpg");
+    }
+
+    create() {
+        this.scene.start("PlayGame");
+    }
+}
+
+class playGame extends Phaser.scene {
+    constructor () {
+        super("PlayGame");
+    }
+    create() {
+
+    }
+}
+
+function resizeGame() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    // ** Wo ist game.config? ** -> config ist eine Eigenschaft der game.
+    var gameRatio = game.config.width / game.config.height;
+    if (windowRatio < gameRatio) {
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    } else {
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
