c5e57f1c3a3e252cf275a7f236d52f580abe1981
evankaulitzky <49273374+evankaulitzky@users.noreply.github.com>
2019-05-17 16:37:58 +0200

    Update game.js
    ich habe nach dem Beispiel von Übung Adding Tween Animation 004 ausprobiert.

--- Game/game.js
+++ Game/game.js
@@ -1,11 +1,12 @@
 var game;
 var gameOptions = {
-   tileSize : 25,
-   tileSpacing : 0,
-   boardSize : {
-       rows : 36, //Reihen -> Y-Achse
-       cols : 36 //Spalten -> X-Achse
-   }
+    tileSize : 25,
+    tileSpacing : 0,
+    boardSize : {
+        rows : 36, //Reihen -> Y-Achse
+        cols : 36 //Spalten -> X-Achse
+    },
+    tweenSpeed: 2000
 }
 
 
@@ -52,18 +53,48 @@ class playGame extends Phaser.Scene {
             for(var j = 0; j < gameOptions.boardSize.cols; j++){
                 var tilePosition = this.getTilePosition(i,j);
                 this.add.image(tilePosition.x, tilePosition.y, "HintergrundBild");
-                this.add.image(tilePosition.x, tilePosition.y, " ");
+                //Veränderung
+                var tile = this.add.sprite(tilePosition.x, tilePosition.y, "tiles", 0);
+                tile.visible = false;
+                this.boardArray[i][j] = {
+                    tileValue: 0,
+                    tileSprite: tile
+                }
+            }      
+        }
+        this.addTile();
+        this.addTile();
+    }
+    addTile(){
+        var emptyTiles = [];
+        for(var i = 0; i < gameOptions.boardSize.rows; i++){
+            for(var j = 0; j < gameOptions.boardSize.cols; j++){
+                if(this.boardArray[i][j].tileValue == 0){
+                    emptyTiles.push({
+                        row: i,
+                        col: j
+                    })
+                }
             }
         }
-    }
-    getTilePosition(row, col){
-        /*var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
+        if(emptyTiles.length > 0){
+            var chosenTile = Phaser.Utils.Array.GetRandom(emptyTiles);
+            this.boardArray[chosenTile.row][chosenTile.col].tileValue = 1;
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.visible = true;
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(0);
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.alpha = 0;
+            this.tweens.add({
+                targets: [this.boardArray[chosenTile.row][chosenTile.col].tileSprite],
+                alpha: 1,
+                duration: gameOptions.tweenSpeed
+            });
+        }
+        getTilePosition(row, col){
+        var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
         var posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
         return new Phaser.Geom.Point(posX, posY);
-        */
     }
-}
-
+        
 function resizeGame() {
     var canvas = document.querySelector("canvas");
     var windowWidth = window.innerWidth;
