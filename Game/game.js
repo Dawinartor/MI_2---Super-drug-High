4d138b2ac47566d8b1cccb6d038eb68cb3063728
Dawinartor <david.melamed@web.de>
2019-05-21 19:41:42 +0200

    Tagesziel erreicht
    Figur kann nun laufen und bisschen springen

--- Game/game.js
+++ Game/game.js
@@ -1,9 +1,11 @@
 var game;
 var spieler;
+var cursors;
 var gameOptions = {
     tileSize : 25,
     tileSpacing : 0,
     boardSize : {
+        //Reihen -> Y-Achse
         rows : 36, //Reihen -> Y-Achse
         cols : 36 //Spalten -> X-Achse
     },
@@ -13,6 +15,7 @@ var gameOptions = {
         playerHeight : 70
     }
 }
+
 
 
 
@@ -51,6 +54,7 @@ class bootGame extends Phaser.Scene {
             frameWidth : gameOptions.SinglePlayerFrame.playerWidth, // X-Wert
             frameHeight : gameOptions.SinglePlayerFrame.playerHeight // Y-Wert
         }); // Sprite-Sheet 
+        this.load.image("Bodenteil", "verwendeteImages/Block.jpg");
     
     }
 
@@ -67,32 +71,79 @@ class playGame extends Phaser.Scene {
     }
 
     create() {
+        
         this.add.image(450, 450, "HintergrundBild");
-        this.boardArray = [];
+        var boden = this.physics.add.staticGroup();
+        boden.create(25, 900, "Bodenteil");
+
+        /*this.boardArray = [];
         for(var i = 0; i < gameOptions.boardSize.rows ; i++){
             this.boardArray[i] = [];
             for(var j = 0; j < gameOptions.boardSize.cols; j++){
-                var tilePosition = this.getTilePosition(i,j);
+                var tilePosition = this.getTilePosition(i,j);*/
                 // this.add.image(tilePosition.x, tilePosition.y, "HintergrundBild");
                 //Statt var tile -> var spieler
-                spieler = this.physics.add.sprite(25, 850, "SpriteSheetLinkeseite", 3);
 
+                // Spieler Position & Physik wird definiert : 
+                spieler = this.physics.add.sprite(25, 850, "SpriteSheetLinkeseite", 0);
+                spieler.setBounce(0.5);
                 spieler.setCollideWorldBounds(true);
+
+                this.anims.create({
+                    key : "left",
+                    frames : this.anims.generateFrameNumbers("SpriteSheetLinkeseite", { start : 0, end : 2 }),
+                    frameRate : 10,
+                     repeat : -1
+                });
+                this.anims.create({
+                    key : "turn",
+                    frames : [{key : "SpriteSheetLinkeseite", frame : 3}],
+                    frameRate : 20,
+                });
+                this.anims.create({
+                    key : "right",
+                    frames : this.anims.generateFrameNumbers("SpriteSheetLinkeseite", { start : 4, end : 6 }),
+                    frameRate : 10,
+                     repeat : -1
+                });
+
+                cursors = this.input.keyboard.createCursorKeys();
+                this.physics.add.collider(spieler, boden);
+
 
 /* Unterschied zwischen unvisible und transparent: 
 * -> Transparent wurde gerendert, nimmt auch Platz ein, mann kann es nur nicht sehen.
    -> Unvisible wurde garnicht gerändert. */
-                spieler.visible = true;
+
+                /*spieler.visible = true;
                 this.boardArray[i][j] = {
                     tileValue: 0,
                     tileSprite: spieler // Unsere Figur
-                }
+                }*/
             }
-        }
+    
        // this.addTile();
 
-    }
 
+    update(){
+
+        if(cursors.left.isDown){
+            spieler.setVelocityX(-160);
+            spieler.anims.play("left", true);
+        }
+        else if(cursors.right.isDown){
+            spieler.setVelocityX(160);
+            spieler.anims.play("right", true);
+        }
+        else{
+            spieler.setVelocityX(0);
+            spieler.anims.play("turn");
+        }
+        // Wenn Spieler Taste dückt && Wenn Figur auf definierten Boden steht
+        if(cursors.up.isDown && spieler.body.touching.down ){
+            spieler.setVelocityY(-330);
+        }
+    }
     getTilePosition(row, col){
         var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
         var posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
@@ -108,6 +159,8 @@ class playGame extends Phaser.Scene {
         return new Phaser.Geom.Rectangle(posX, posY, rectangleWidth, rectangleheight); // Rechteck wird erzeugt -> Unser Spieler.
 
     }
+
+
 
 /*
     addTile(){
@@ -140,7 +193,8 @@ class playGame extends Phaser.Scene {
         }
 */
 
-    }
+   }
+
 
 
 
