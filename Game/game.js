2841c798ec5ad4b75079cb298306613eb65ea68c
Dawinartor <david.melamed@web.de>
2019-05-31 21:01:04 +0200

    Änderungen Übernommen

--- Game/game.js
+++ Game/game.js
@@ -1,7 +1,8 @@
 var game;
+var item0;
+var item1;
+var item2;
 var abstandX;
-var bgY = 450;
-var bgX = 300;
 var spieler;
 var bodenStueck;
 var cursors;
@@ -13,7 +14,7 @@ var gameOptions = {
     tileSpacing : 0,
     boardSize : {
         maxHeight : 900, // Um die Welt zu definieren
-        maxWidth : 900, // Um die Welt zu definieren
+        maxWidth : 2700, // Um die Welt zu definieren
         //Reihen -> Y-Achse
         height : 36, //Reihen -> Y-Achse
         width : 36 //Spalten -> X-Achse
@@ -65,10 +66,15 @@ class bootGame extends Phaser.Scene {
         // Die Skyline -> Hintergrund der geladen wird.
         this.load.image("HintergrundBild", "verwendeteImages/Background-Town.png");
         this.load.image("Bodenteil", "verwendeteImages/Block.jpg");
+        // Items zum einsammeln
+        this.load.image("item", "verwendeteImages/Items/Fliese2.png");
+        this.load.image("Itemgruen","verwendeteImages/Items/Item-gruen.png");
+        this.load.image("Itemrot","verwendeteImages/Items/Item-rot.png");
         this.load.spritesheet("SpriteSheetLinkeseite","verwendeteImages/SpriteSheet/junky_normal sprite-sheet.png", {
             frameWidth : gameOptions.SinglePlayerFrame.playerWidth, // X-Wert
             frameHeight : gameOptions.SinglePlayerFrame.playerHeight // Y-Wert
-        }); // Sprite-Sheet 
+        }); 
+        // Sprite-Sheet 
         this.load.image("red", "verwendeteImages/Fliese2.png");
     }
 
@@ -78,6 +84,7 @@ class bootGame extends Phaser.Scene {
         // TODO - Write Update scene
     }
 }
+
 
 // PlayGame - Klasse -----------------------------------------------
 
@@ -89,17 +96,68 @@ class playGame extends Phaser.Scene {
 
 
     create() {
+        //hintergrund wird erzeugt
         hintergrund = this.add.image(gameOptions.mapSize.bgX , gameOptions.mapSize.bgY, "HintergrundBild");
-        //hintergrund
+        //boden wird erzeugt
         bodenStueck = this.physics.add.staticGroup();
-       // this.bauePlatformX25();
-       // this.baueEinfachePlatformRandom();
-       //this.bauePlatformMitParameter(99);
+        // Item wird Physik hinzugefügt
+        item = this.physics.add.group({
+            key : 'item',
+            repeat : 3,
+            setXY : { 
+                x : 12,
+                y : 0,
+                stepX : 200
+                    }
+        });
+
+        item2 = this.physics.add.group({
+            key : 'Itemrot',
+            repeat : 2,
+            setXY : { 
+                x : 12,
+                y : 0,
+                stepX : 200
+                    }
+        });
+
+        item3 = this.physics.add.group({
+            key : 'Itemgruen',
+            repeat : 3,
+            setXY : { 
+                x : 12,
+                y : 0,
+                stepX : 200
+                    }
+        });
+
+
+
       
         // Spieler Position & Physik wird definiert : 
         spieler = this.physics.add.sprite(25, 850, "SpriteSheetLinkeseite", 0);
         spieler.setBounce(0.3);
         spieler.setCollideWorldBounds(true);
+        // Item als platzierbarer Gegenstand in der Welt
+        item.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            child.setCollideWorldBounds(true);
+            child.setScale(0.5);
+        });
+
+        item2.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            child.setCollideWorldBounds(true);
+            child.setScale(0.5);
+        });
+
+        item3.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            child.setCollideWorldBounds(true);
+            child.setScale(0.5);
+        });
+
+
 
             this.anims.create({
                 key : "left",
@@ -118,33 +176,38 @@ class playGame extends Phaser.Scene {
                 frameRate : 10,
                     repeat : -1
             });
-        
+            // Item wird als Objekt definiert
             items = this.physics.add.group({
             key: 'red',
             repeat: 0,
             setXY: { x: 200, y: 0, stepX: 100 }
             });
-        
-            items.children.iterate(function (child) {
-                child.setCollideWorldBounds(true);
-                child.setScale(0.5);
 
-                //child.setBounceY(Phaser.Math.FloatBetween(0, 0));
+            
 
-            });
         
-            this.cameras.main.startFollow(spieler, true, 0.08, 0.08);
+            this.cameras.main.startFollow(spieler, true);
             this.cameras.main.setBounds(0, 0, 420 * 2, 176);
             cam = this.cameras.main;
             //this.cameras.main.setScrollX(spieler, true, 0.08, 0.08);
             //this.camera.marginBottom.startsWith()
             //this.cameras.main.setZoom(9);
             cursors = this.input.keyboard.createCursorKeys();
+            // ?? *************************************** ??
             this.physics.add.collider(items, bodenStueck);
+            // ?? *************************************** ??
+            // Items werden Collisionsabfrage hinzugefügt
+            this.physics.add.collider(item, bodenStueck);
+            this.physics.add.collider(item2, bodenStueck);
+            this.physics.add.collider(item3, bodenStueck);
             this.physics.add.collider(spieler, bodenStueck);
+            // Möglichkeit wird hinzugefügt, dass Spieler das Item einsammeln kann 
+            this.physics.add.overlap(spieler, item, this.sammelItem, null, this);
+            this.physics.add.overlap(spieler, item2, this.sammelItem, null, this);
+            this.physics.add.overlap(spieler, item3, this.sammelItem, null, this);
             this.cameras.main.setBounds(0, 0, gameOptions.mapSize.bgX, gameOptions.mapSize.bgY);
             this.cameras.main.startFollow(spieler);
-            this.physics.add.overlap(spieler, items, collectItemsOne, null, this);
+            
         }
     
 
@@ -160,12 +223,14 @@ class playGame extends Phaser.Scene {
             spieler.anims.play("left", true);
         }
         else if(cursors.right.isDown){
-            spieler.setVelocityX(60);
-            cam.x-=0.5;
+            spieler.setVelocityX(10);
+          //  cam.x -= 0.5;
+          // item.setVelocityX(-10);
             spieler.anims.play("right", true);
             
         }
-        else{
+        else {
+         // items.setVelocityX(0);
             spieler.setVelocityX(0);
             spieler.anims.play("turn");
         }
@@ -178,7 +243,7 @@ class playGame extends Phaser.Scene {
             this.scene.start("PlayGame1");
         }
         else{
-           console.log("SpasstPos : " + spieler.x + " " + spieler.y);
+           // console.log("SpasstPos : " + spieler.x + " " + spieler.y);
         }
      }
 
@@ -206,8 +271,6 @@ class playGame extends Phaser.Scene {
     }*/
 
         baueEinfachePlatformRandom() {
-        let positionY_boden = 0;
-        let positionX_boden = 0;
         var randomNummber;
             
         for ( let i = 0; i < 900; i += 50) {// i = Anzahl der Steine auf dem Feld. 
@@ -231,11 +294,11 @@ class playGame extends Phaser.Scene {
             }
         }
     }
-    
-    collectItemsOne(spieler, red){
-        red.disableBody(true, true);
-    }
 
+
+    sammelItem(spieler, item) {
+        item.disableBody(true, true);
+    }
     
 }
 
@@ -252,7 +315,7 @@ class playGame1 extends Phaser.Scene {
         console.log("level1");
         hintergrund = this.add.image(gameOptions.mapSize.bgX , gameOptions.mapSize.bgY, "HintergrundBild");
         //hintergrund
-        bodenStueck = this.physics.add.staticGroup();
+        bodenStueck = this.physics.add.staticGroup(); // Staticgroup unveränderbare Teile des Spiels
         this.bauePlatformX25();
         //this.bauePlatformRandom_X_Y();
       
@@ -279,7 +342,7 @@ class playGame1 extends Phaser.Scene {
                     repeat : -1
             });
             this.cameras.main.startFollow(spieler, true, 0.08, 0.08);
-            this.cameras.main.setBounds(0, 0, 420 * 2, 176);
+            this.cameras.main.setBounds(0, 0, 1350 * 2, 176);
             cam = this.cameras.main;
             //this.cameras.main.setScrollX(spieler, true, 0.08, 0.08);
             //this.camera.marginBottom.startsWith()
